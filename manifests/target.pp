b14df8d6f2c818ddc924049d9e0da386c350ca66
David Schmitt <david@dasz.at>
2013-06-25 09:44:23 +0200

    reformat

--- manifests/target.pp
+++ manifests/target.pp
@@ -7,11 +7,8 @@
 # Usage:
 # include icinga::target
 #
-class icinga::target (
-  $host_template = 'generic-host'
-  ) {
-
-  # This variable defines where icinga automatically generated
+class icinga::target ($host_template = 'generic-host') {
+  # This variable defines where icinga automatically generated
   # files are places. This MUST be the same of $::icinga::customconfigdir
   # HINT: Do not mess with default path names...
 
@@ -28,21 +25,15 @@ class icinga::target (
   # TODO: Find a smarter solution that doesn't require TopScope Variables
   $magic_hostgroup = get_magicvar($::icinga_hostgrouplogic)
 
-  icinga::host { $::fqdn:
-    use => $host_template,
-  }
+  icinga::host { $::fqdn: use => $host_template, }
 
-  icinga::baseservices { $::fqdn:
-    use => 'generic-service',
-  }
+  icinga::baseservices { $::fqdn: use => 'generic-service', }
 
-## TODO Make this work with nagios::plugins
-#   include icinga::plugins
+  # TODO: Make this work with nagios::plugins
+  # include icinga::plugins
 
-# Automatic hostgroup management
+  # Automatic hostgroup management
   if $::icinga_hostgrouplogic {
-    icinga::hostgroup { $::fqdn:
-      hostgroup => $magic_hostgroup,
-    }
+    icinga::hostgroup { $::fqdn: hostgroup => $magic_hostgroup, }
   }
 }
